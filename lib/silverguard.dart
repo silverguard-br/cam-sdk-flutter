dd55830c961b8da3c9c9acbb3524ea0dce6852b3
Marcos Luiz <marcosluizss@gmail.com>
2025-10-02 15:41:26 -0300

    feat: add theme function e pass variable

--- lib/silverguard.dart
+++ lib/silverguard.dart
@@ -2,3 +2,4 @@ export 'src/silverguard_cam.dart';
 export 'src/silverguard/model/request_list_url_model.dart';
 export 'src/silverguard/model/request_url_model.dart';
 export 'src/silverguard/silverguard_bridge.dart';
+export 'src/silverguard/silverguard_theme.dart';
