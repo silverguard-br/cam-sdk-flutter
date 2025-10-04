785f4663cefa7e6bac31a072e71931a28845ea26
Marcos Luiz <marcosluizss@gmail.com>
2025-10-04 14:10:31 -0300

    chore: update theme class

--- lib/src/silverguard/silverguard_theme.dart
+++ lib/src/silverguard/silverguard_theme.dart
@@ -12,21 +12,43 @@ class SilverguardTheme {
 }
 
 class SilverguardThemeTextStyles {
-  TextStyle get button => TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
-  TextStyle get body => TextStyle(fontSize: 14);
-  TextStyle get headline2 => TextStyle(fontSize: 24);
-  TextStyle get headline3 => TextStyle(fontSize: 20);
+  final TextStyle button;
+  final TextStyle body;
+  final TextStyle headline2;
+  final TextStyle headline3;
+
+  SilverguardThemeTextStyles({
+    TextStyle? button,
+    TextStyle? body,
+    TextStyle? headline2,
+    TextStyle? headline3,
+  }) : button = button ?? TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+       body = body ?? TextStyle(fontSize: 14),
+       headline2 = headline2 ?? TextStyle(fontSize: 24),
+       headline3 = headline3 ?? TextStyle(fontSize: 20);
 }
 
 class SilverguardThemeColors {
-  Color get background => Color(0xFFFFFFFF);
+  final Color background;
+  final Color primary;
+  final Color label;
+  final Color buttonTitle;
+  final Color buttonEnabled;
+  final Color buttonDisabled;
 
-  Color get primary => Color(0xFF1B264F);
+  SilverguardThemeColors({
+    Color? background,
+    Color? primary,
+    Color? label,
+    Color? buttonTitle,
+    Color? buttonEnabled,
+    Color? buttonDisabled,
+  }) : background = background ?? Color(0xFFFFFFFF),
+       primary = primary ?? Color(0xFF1B264F),
+       label = label ?? Color(0xFF282828),
+       buttonTitle = buttonTitle ?? Color(0xFFFEFEFE),
+       buttonEnabled = buttonEnabled ?? Color(0xFF1B264F),
+       buttonDisabled = buttonDisabled ?? Color(0xFF767D95);
+
   Color get primary04 => primary.withAlpha(40);
-
-  Color get label => Color(0xFF282828);
-
-  Color get buttonTitle => Color(0xFFFEFEFE);
-  Color get buttonEnabled => Color(0xFF1B264F);
-  Color get buttonDisabled => Color(0xFF767D95);
 }
