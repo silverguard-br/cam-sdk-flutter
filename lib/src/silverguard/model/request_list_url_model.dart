a278655c0006645d067252518982f89440cbc528
Marcos Luiz <marcosluizss@gmail.com>
2025-10-08 21:29:04 -0300

    feat: add branch e account number on models

--- lib/src/silverguard/model/request_list_url_model.dart
+++ lib/src/silverguard/model/request_list_url_model.dart
@@ -1,9 +1,21 @@
 class RequestListUrlModel {
   final String reporterClientId;
+  final String? reporterBranchNumber;
+  final String? reporterAccountNumber;
 
-  const RequestListUrlModel({required this.reporterClientId});
+  const RequestListUrlModel({
+    required this.reporterClientId,
+    this.reporterBranchNumber,
+    this.reporterAccountNumber,
+  });
 
   Map<String, String> toJson() {
-    return {"reporter_client_id": reporterClientId};
+    return {
+      "reporter_client_id": reporterClientId,
+      if (reporterBranchNumber != null)
+        "reporter_branch_number": reporterBranchNumber!,
+      if (reporterAccountNumber != null)
+        "reporter_account_number": reporterAccountNumber!,
+    };
   }
 }
